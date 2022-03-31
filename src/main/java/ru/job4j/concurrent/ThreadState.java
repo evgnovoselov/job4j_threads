e21e818cb4d0f4193e9d1d5287bc797d646c0216
Evgeny Novoselov <zkodru@gmail.com>
2022-03-31 16:21:32 +0300

    Сделал постоянный вывод состояния

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -11,16 +11,21 @@ public class ThreadState {
         Thread second = new Thread(() -> {
             System.out.println(Thread.currentThread().getName());
         });
-        System.out.printf("%s : %s%n", first.getState(), second.getState());
+        System.out.println("cur " + Thread.currentThread().getState());
+        System.out.println("first " + first.getState());
+        System.out.println("second " + second.getState());
+        System.out.println();
         first.start();
         second.start();
-        int count = 0;
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
-            System.out.printf("%s : %s%n", first.getState(), second.getState());
-            count++;
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println("cur " + Thread.currentThread().getState());
+            System.out.println("first " + first.getState());
+            System.out.println("second " + second.getState());
+            System.out.println();
         }
-        System.out.printf("%s : %s%n", first.getState(), second.getState());
-        System.out.println(count);
-        System.out.println("Завершение работы программы");
+        System.out.println("cur " + Thread.currentThread().getState());
+        System.out.println("first " + first.getState());
+        System.out.println("second " + second.getState());
+        System.out.println();
     }
 }
