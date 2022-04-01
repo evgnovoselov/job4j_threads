c91052196a2eb245eca54f9f5254c0697c603b9e
Evgeny Novoselov <zkodru@gmail.com>
2022-04-01 16:20:39 +0300

    Начал реализацию прерывание потока

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,7 +1,30 @@
 package ru.job4j.concurrent;
 
 /**
- * Прогресс в консоле.
+ * Прерывание прогресса в консоле.
  */
-public class ConsoleProgress {
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        char[] process = new char[]{'\\', '|', '/'};
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rLoading: " + process[index++]);
+            if (index == process.length - 1) {
+                index = 0;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
 }
