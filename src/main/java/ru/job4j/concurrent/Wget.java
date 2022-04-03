fa571c51ea1eb90cdf439fec692d57c3dda291ad
Evgeny Novoselov <zkodru@gmail.com>
2022-04-03 16:52:55 +0300

    Сделал скачивание файла.

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,20 +1,48 @@
 package ru.job4j.concurrent;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
 /**
- * Класс эмулирует вывод прогресса загрузки.
+ * Класс загружает файл и выводит прогресса загрузки.
  */
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            for (int index = 0; index <= 100; index++) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.printf("\rLoading : %d %%", index);
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream out = new FileOutputStream("files/tmp/pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[speed];
+            int byteRead;
+            while ((byteRead = in.read(dataBuffer, 0, speed)) != -1) {
+                out.write(dataBuffer, 0, byteRead);
+                Thread.sleep(1000);
             }
-        });
-        thread.start();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        int speed = 1024;
+        if (args.length > 2) {
+            url = args[0];
+            speed = Integer.parseInt(args[1]);
+        }
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
