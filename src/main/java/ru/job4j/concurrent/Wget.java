9bf6db186457cfe3e15407220a524b4a428621e6
Evgeny Novoselov <zkodru@gmail.com>
2022-04-03 18:00:56 +0300

    Revert "4. Скачивание файла с ограничением. [#144271] (#7)" (#8)
    This reverts commit 27ee0fb09441ef272081e891b2754ee99f0b1a55.

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,48 +1,20 @@
 package ru.job4j.concurrent;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URL;
-
 /**
- * Класс загружает файл и выводит прогресса загрузки.
+ * Класс эмулирует вывод прогресса загрузки.
  */
-public class Wget implements Runnable {
-    private final String url;
-    private final int speed;
-
-    public Wget(String url, int speed) {
-        this.url = url;
-        this.speed = speed;
-    }
-
-    @Override
-    public void run() {
-        Thread progress = new Thread(new ConsoleProgress());
-        progress.start();
-        String fileName = url.substring(url.lastIndexOf("/") + 1);
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("files/tmp/" + fileName)) {
-            byte[] dataBuffer = new byte[speed];
-            int byteRead;
-            while ((byteRead = in.read(dataBuffer, 0, speed)) != -1) {
-                out.write(dataBuffer, 0, byteRead);
-                Thread.sleep(1000);
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            for (int index = 0; index <= 100; index++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("\rLoading : %d %%", index);
             }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        progress.interrupt();
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
-        wget.start();
-        wget.join();
+        });
+        thread.start();
     }
 }
