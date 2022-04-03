fd07124d5733dd6e83dea400f471b0897ac447a9
Evgeny Novoselov <zkodru@gmail.com>
2022-04-03 18:44:48 +0300

    Реализовал загрузку файла

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,42 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        String fileName = url.substring(url.lastIndexOf("/") + 1);
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream out = new FileOutputStream("files/tmp/" + fileName)) {
+            byte[] dataBuffer = new byte[speed];
+            int byteRead;
+            while ((byteRead = in.read(dataBuffer, 0, speed)) != -1) {
+                out.write(dataBuffer, 0, byteRead);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
