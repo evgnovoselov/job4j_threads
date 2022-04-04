7153d8279b07fa522724298cf1bc7577a63b087a
Evgeny Novoselov <zkodru@gmail.com>
2022-04-04 11:51:38 +0300

    4. Скачивание файла с ограничением. [#144271 #283911] fix (#12)

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -6,9 +6,10 @@ import java.io.IOException;
 import java.net.URL;
 
 /**
- * Загрузка файла.
+ * Загрузка файла с ограничением по скорости.
  */
 public class Wget implements Runnable {
+    private static final int LENGTH_BUFFER = 1024;
     private final String url;
     private final int speed;
 
@@ -21,12 +22,27 @@ public class Wget implements Runnable {
     public void run() {
         String fileName = url.substring(url.lastIndexOf("/") + 1);
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("files/tmp/" + fileName)) {
-            byte[] dataBuffer = new byte[speed];
+             FileOutputStream out = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[LENGTH_BUFFER];
             int byteRead;
-            while ((byteRead = in.read(dataBuffer, 0, speed)) != -1) {
+            long bytesWritten = 0;
+            long startTime = System.currentTimeMillis();
+            long deltaTime;
+            int sleep;
+            while ((byteRead = in.read(dataBuffer, 0, LENGTH_BUFFER)) != -1) {
                 out.write(dataBuffer, 0, byteRead);
-                Thread.sleep(1000);
+                bytesWritten += LENGTH_BUFFER;
+                if (bytesWritten >= speed) {
+                    deltaTime = System.currentTimeMillis() - startTime;
+                    if (deltaTime < 1000) {
+                        sleep = (int) (1000 - deltaTime);
+                    } else {
+                        sleep = 0;
+                    }
+                    Thread.sleep(sleep);
+                    startTime = System.currentTimeMillis();
+                    bytesWritten = 0;
+                }
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -36,10 +52,26 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        verification(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();
+    }
+
+    private static void verification(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Not set args. Need set args: <url> <speed>. Where speed - byte/s.");
+        }
+        int speed;
+        try {
+            speed = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Not valid arg <speed>. Need set args: <url> <speed>. Where speed - byte/s.");
+        }
+        if (speed < 1) {
+            throw new IllegalArgumentException("Not valid arg <speed>. Need set number more 1 byte/s.");
+        }
     }
 }
