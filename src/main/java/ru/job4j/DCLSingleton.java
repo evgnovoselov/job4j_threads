c9410b937ff77b32208e4d6d004174997b85a172
Evgeny Novoselov <zkodru@gmail.com>
2022-04-05 13:10:19 +0300

    2. Модель памяти Java [#267917] (#17)

---
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+/**
+ * Потокобезопасный Double check locking Singleton.
+ */
+public final class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+}
