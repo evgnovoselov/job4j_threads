d5e1e8cd42ef4bb16d0201793605fb630f145000
Evgeny Novoselov <zkodru@gmail.com>
2022-04-05 21:01:36 +0300

    Поправил потокобезопасность

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -4,14 +4,14 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class UserCache {
-    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<Integer, User>();
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger();
 
     public void add(User user) {
-        users.put(id.incrementAndGet(), user);
+        users.put(id.incrementAndGet(), User.of(user.getName()));
     }
 
     public User findById(int id) {
-        return users.get(id);
+        return User.of(users.get(id).getName());
     }
 }
