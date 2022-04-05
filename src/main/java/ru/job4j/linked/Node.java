e47b4483b71a4ef0d8446858f250a8cec2caef71
Evgeny Novoselov <zkodru@gmail.com>
2022-04-05 20:05:12 +0300

    Добавил реализацию потокобезопасного узла

--- src/main/java/ru/job4j/linked/Node.java
+++ src/main/java/ru/job4j/linked/Node.java
@@ -1,22 +1,24 @@
 package ru.job4j.linked;
 
+/**
+ * Потокобезопасный класс, описывающий узел односвязного списка.
+ *
+ * @param <T>
+ */
 public class Node<T> {
-    private Node<T> next;
-    private T value;
+    private final T value;
+    private final Node<T> next;
+
+    public Node(T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
 
     public Node<T> getNext() {
         return next;
     }
 
-    public void setNext(Node<T> next) {
-        this.next = next;
-    }
-
     public T getValue() {
         return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
     }
 }
