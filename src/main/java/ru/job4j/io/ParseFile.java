533adb5bddcdd41c9d08224ce6e9f499c2fda231
Evgeny Novoselov <zkodru@gmail.com>
2022-04-06 21:04:20 +0300

    Поправил первый метод getContent

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -13,21 +13,24 @@ public class ParseFile {
         return file;
     }
 
-    public String getContent() throws IOException {
-        InputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            output += (char) data;
+    public String getContent() {
+        StringBuilder output = new StringBuilder();
+        try (InputStream in = new FileInputStream(file)) {
+            int data;
+            while ((data = in.read()) > 0) {
+                output.append((char) data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return output;
+        return output.toString();
     }
 
     public String getContentWithoutUnicode() throws IOException {
-        InputStream i = new FileInputStream(file);
+        InputStream in = new FileInputStream(file);
         String output = "";
         int data;
-        while ((data = i.read()) > 0) {
+        while ((data = in.read()) > 0) {
             if (data < 0x80) {
                 output += (char) data;
             }
@@ -36,9 +39,9 @@ public class ParseFile {
     }
 
     public void saveContent(String content) throws IOException {
-        OutputStream o = new FileOutputStream(file);
+        OutputStream out = new FileOutputStream(file);
         for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+            out.write(content.charAt(i));
         }
     }
 }
