f0ed8e70329d3873cba940b074752f41b677963d
Evgeny Novoselov <zkodru@gmail.com>
2022-04-07 09:42:27 +0300

    2. JCIP. Настройка библиотеки [#268575] (#23)

---
+++ src/test/java/ru/job4j/jcip/CountTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.jcip;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CountTest {
+    /**
+     * Класс описывает нить со счетчиком.
+     */
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count();
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Assert.assertEquals(2, count.get());
+    }
+}
