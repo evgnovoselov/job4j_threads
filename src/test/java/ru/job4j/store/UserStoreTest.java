29875f6d8108960650dbfaeb92d349433d9e06db
Evgeny Novoselov <zkodru@gmail.com>
2022-04-07 12:46:06 +0300

    Добавил тесты

--- src/test/java/ru/job4j/store/UserStoreTest.java
+++ src/test/java/ru/job4j/store/UserStoreTest.java
@@ -7,6 +7,9 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class UserStoreTest {
+    /**
+     * Проверяем добавление пользователей с одинаковыми id.
+     */
     @Test
     public void whenAddUsersSameIdThenNotAddSecondUser() {
         Store store = new UserStore();
@@ -14,12 +17,35 @@ public class UserStoreTest {
         assertFalse(store.add(new User(1, 200)));
     }
 
+    /**
+     * Проверяем получение всех пользователей.
+     */
     @Test
     public void whenAdd2UsersAndFindAllThenGetUsers() {
         Store store = new UserStore();
         store.add(new User(1, 50));
         store.add(new User(2, 200));
         assertEquals(List.of(new User(1, 100), new User(2, 200)), store.findAll());
+    }
+
+    /**
+     * Проверяем обновление пользователя в хранилище.
+     */
+    @Test
+    public void whenUpdateHaveUserThenUpdate() {
+        Store store = new UserStore();
+        store.add(new User(1, 100));
+        assertTrue(store.update(new User(1, 500)));
+        assertEquals(500, store.findById(1).orElseThrow().getAmount());
+    }
+
+    /**
+     * Проверяем обновление пользователя которого нет в хранилище.
+     */
+    @Test
+    public void whenUpdateNotHaveUserThenNotUpdate() {
+        Store store = new UserStore();
+        assertFalse(store.update(new User(2, 500)));
     }
 
     @Test
