9d73e4dd0b1bf676df8f5ee8a889a6450f0f47bb
Evgeny Novoselov <zkodru@gmail.com>
2022-04-09 22:15:51 +0300

    Поправил реализацию

--- src/main/java/ru/job4j/wait/producerconsumer/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/wait/producerconsumer/SimpleBlockingQueue.java
@@ -22,32 +22,27 @@ public class SimpleBlockingQueue<T> {
     }
 
     public synchronized void offer(T value) {
-        while (!Thread.interrupted() && maxSize > 0 && queue.size() >= maxSize) {
+        while (maxSize > 0 && queue.size() >= maxSize) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
-        if (!Thread.interrupted()) {
-            queue.offer(value);
-            notifyAll();
-        }
+        queue.offer(value);
+        notifyAll();
     }
 
     public synchronized T poll() {
-        T result = null;
-        while (!Thread.interrupted() && queue.isEmpty()) {
+        while (queue.isEmpty()) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
-        if (!Thread.interrupted()) {
-            result = queue.poll();
-            notifyAll();
-        }
+        T result = queue.poll();
+        notifyAll();
         return result;
     }
 }
