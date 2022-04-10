5cccaaa7bda87096ca156fe998843314faaacab0
Evgeny Novoselov <zkodru@gmail.com>
2022-04-10 20:24:26 +0300

    Частично вернул шаблон

--- src/main/java/ru/job4j/wait/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/wait/buffer/ParallelSearch.java
@@ -6,32 +6,26 @@ public class ParallelSearch {
     public static void main(String[] args) {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
         final Thread consumer = new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
+            while (true) {
                 try {
                     System.out.println(queue.poll());
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        });
-        consumer.start();
-        final Thread producer = new Thread(() -> {
-            for (int index = 0; index < 3; index++) {
-                try {
-                    queue.offer(index);
-                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                     Thread.currentThread().interrupt();
                 }
             }
         });
-        producer.start();
-        try {
-            producer.join();
-        } catch (InterruptedException e) {
-            producer.interrupt();
-        }
+        consumer.start();
+        new Thread(() -> {
+            for (int index = 0; index < 3; index++) {
+                try {
+                    queue.offer(index);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
         while (!consumer.isInterrupted() && consumer.getState() != Thread.State.WAITING) {
             try {
                 Thread.sleep(1000);
