5fd6baaf741dea85c2d6bc86b0b681328cf4ccf2
Evgeny Novoselov <zkodru@gmail.com>
2022-04-10 10:46:19 +0300

    Добавил реализацию

--- src/main/java/ru/job4j/wait/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/wait/buffer/ParallelSearch.java
@@ -1,7 +1,31 @@
 package ru.job4j.wait.buffer;
 
+import ru.job4j.wait.producerconsumer.SimpleBlockingQueue;
+
 public class ParallelSearch {
     public static void main(String[] args) {
-        
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println(queue.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        consumer.start();
+        new Thread(() -> {
+            for (int index = 0; index < 3; index++) {
+                try {
+                    queue.offer(index);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
     }
 }
