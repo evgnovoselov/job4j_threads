18c7aeee33fce36016812d48c55ae1785e5dc86a
Evgeny Novoselov <zkodru@gmail.com>
2022-04-11 22:25:40 +0300

    Начал делать метод обновления

--- src/main/java/ru/job4j/nonblock/cache/Cache.java
+++ src/main/java/ru/job4j/nonblock/cache/Cache.java
@@ -1,17 +1,25 @@
 package ru.job4j.nonblock.cache;
 
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@ThreadSafe
 public class Cache {
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
 
     public boolean add(Base model) {
-        return false;
+        return memory.putIfAbsent(model.getId(), model) == null;
     }
 
     public boolean update(Base model) {
-        return false;
+        return memory.computeIfPresent(model.getId(), (integer, base) -> {
+            Base stored = memory.get(model.getId());
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal.");
+            }
+        });
     }
 
     public void delete(Base model) {
