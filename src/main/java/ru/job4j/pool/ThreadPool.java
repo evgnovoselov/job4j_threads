c8e122498833c4ff5fc7fc04a2a962a48ed1874e
Evgeny Novoselov <zkodru@gmail.com>
2022-04-13 09:41:22 +0300

    Поправил инициализацию пула

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -4,10 +4,27 @@ import ru.job4j.wait.producerconsumer.SimpleBlockingQueue;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(Runtime.getRuntime().availableProcessors());
+    private final SimpleBlockingQueue<Runnable> tasks;
+
+    public ThreadPool() {
+        this(1000);
+    }
+
+    public ThreadPool(int limitQueue) {
+        tasks = new SimpleBlockingQueue<>(limitQueue);
+        int size = Runtime.getRuntime().availableProcessors();
+        IntStream.range(0, size).forEach(value -> threads.add(new Thread(() -> {
+            try {
+                tasks.poll().run();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        })));
+    }
 
     /**
      * Добавляем задачи на выполнение.
