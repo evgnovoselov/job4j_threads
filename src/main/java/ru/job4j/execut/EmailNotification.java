5ae22b0a8c8265dc5d464d14762efb41fdf45614
Evgeny Novoselov <zkodru@gmail.com>
2022-04-13 19:35:42 +0300

    Создал классы отправки почты и пользователя

---
+++ src/main/java/ru/job4j/execut/EmailNotification.java
@@ -0,0 +1,18 @@
+package ru.job4j.execut;
+
+/**
+ * Сервис отправки почты.
+ */
+public class EmailNotification {
+    public void emailTo(User user) {
+
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close() {
+
+    }
+}
---
+++ src/main/java/ru/job4j/execut/User.java
@@ -0,0 +1,4 @@
+package ru.job4j.execut;
+
+public class User {
+}
