27bd9d378c3aea4206699e31058f60f596bed3ad
Evgeny Novoselov <zkodru@gmail.com>
2022-04-13 21:32:48 +0300

    Реализовал сервис для рассылки почты, через хранилище нитей

--- src/main/java/ru/job4j/execut/EmailNotification.java
+++ src/main/java/ru/job4j/execut/EmailNotification.java
@@ -1,18 +1,31 @@
 package ru.job4j.execut;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Сервис отправки почты.
  */
 public class EmailNotification {
-    public void emailTo(User user) {
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
+    public void emailTo(User user) {
+        String subject = String.format("Notification %s to email %s.", user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event to %s.", user.getUsername());
+        pool.submit(() -> send(subject, body, user.getEmail()));
     }
 
     public void send(String subject, String body, String email) {
-
     }
 
     public void close() {
-
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
