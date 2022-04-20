bf4e2d6d9992ac44e4abe615697af0400c29b94c
Evgeny Novoselov <zkodru@gmail.com>
2022-04-20 22:10:24 +0300

    Добавил класс теста

---
+++ src/test/java/ru/job4j/async/RolColSumTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.async;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Тестирования класса, который считает суммы по строкам и столбцам квадратной матрицы.
+ */
+public class RolColSumTest {
+    /**
+     * TODO Реализовать тест.
+     */
+    @Test
+    public void sum() {
+        assertTrue(true);
+    }
+
+    /**
+     * TODO Реализовать тест.
+     */
+    @Test
+    public void asyncSum() {
+        assertTrue(true);
+    }
+}
