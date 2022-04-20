4399ee29a7b7fefa267f14073548a3649820e13f
Evgeny Novoselov <zkodru@gmail.com>
2022-04-20 22:08:17 +0300

    Добавил пустые методы и вложенный класс

--- src/main/java/ru/job4j/async/RolColSum.java
+++ src/main/java/ru/job4j/async/RolColSum.java
@@ -4,4 +4,32 @@ package ru.job4j.async;
  * Класс считает суммы по строкам и столбцам квадратной матрицы.
  */
 public class RolColSum {
+    public static class Sums {
+        private int rowSum;
+        private int colSum;
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+    }
+
+    public static Sums[] sum(int[][] matrix) {
+        return new Sums[0];
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) {
+        return new Sums[0];
+    }
 }
